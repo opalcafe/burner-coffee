b5f38d8ab5e904e2ffa251e408bbf37d1a548ca7
PhoenixShell <app.phoenixshell@gmail.com>
2022-04-11 19:53:27 +1000

    Initial commit

---
+++ app/src/main/java/xyz/cloudcoffee/myapplicationburner/BurnerLibrary.kt
@@ -0,0 +1,10 @@
+package xyz.cloudcoffee.myapplicationburner
+
+class BurnerLibrary {
+
+    companion object {
+        fun createString(number : Int): String {
+            return "Number = $number"
+        }
+    }
+}
